04a793910cc7d0ba5bef2b8cf03d833de370cf61
Leo Campbell <leocampbell@Leos-MacBook-Pro.local>
2017-02-13 18:10:00 +0000

    Converting to Rails 5, tests pass. Some issues with rbenv and gem versions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-
   root to: 'favorites#guess'
   get '/guess', to: 'favorites#guess', as: 'guess'
   post '/favorite_language', to: 'favorites#favorite_language', as: 'favorite_language'
 
   get 'html_test', to: 'favorites#unboxed_html_test'
-
 end
