882399d694792ba5c17ab7634501d72a8201c8fc
Leo Campbell <leocampbell@Leos-MacBook-Pro.local>
2017-02-13 19:31:57 +0000

    Got the template working, was not using the expect{} with the curlybrackets

--- spec/guess_spec.rb
+++ spec/guess_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require 'octokit'
 require 'guess_favorite_language'
+require 'pry'
 
 describe GuessFavoriteLanguage do
   describe '#github_username' do
@@ -9,6 +10,23 @@ describe GuessFavoriteLanguage do
       favorite_language = GuessFavoriteLanguage.github_username('leo-ajc')
       expect(favorite_language).to eq('Ruby')
       # Could add a http mocking gem like VCR here to speed up the tests.
+    end
+
+    context 'Testing exception conditions' do
+      subject do
+        GuessFavoriteLanguage.github_username('leo-ajc')
+      end
+
+      it 'raise an exception when the Github user is not found' do
+        allow(Octokit).to receive(:user).and_raise(
+          Octokit::NotFound
+        )
+        expect {
+          subject
+        }.to raise_error(
+          GuessFavoriteLanguage::Error::GithubUserNotFound
+        )
+      end
     end
 
   end
